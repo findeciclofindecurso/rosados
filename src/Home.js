16b8cbe6f4e49af4530d79e16a33e840b8ea4db7
Frank Leal <franklealorg@gmail.com>
2019-06-08 17:48:41 -0500

    well coding

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <AppNavbar/>
+        <AppNavbar />
         <Container fluid>
           <Button color="link"><Link to="/admins">Admin Management</Link></Button>
         </Container>
